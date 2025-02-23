d538b281f76b6794e768c20fbd4e8268f9ff787c
Naeem Sarker <naeemsarker85@gmail.com>
2025-02-24 00:33:21 +0600

    feat(codeforces): solved easy_problem

---
+++ codeforces/Easy_Problem.cpp
@@ -0,0 +1,18 @@
+// https://codeforces.com/contest/2044/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void slv() {
+    int n;
+    cin >> n;
+
+    cout << n-1 << "\n";
+}
+
+int main() {
+    int T;
+    cin >> T;
+
+    while(T--) slv();
+}
